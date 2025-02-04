d5eb028ffafd7f82e2aa804a650b2744c3f871ed
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-02-03 22:38:54 -0300

    Done

---
+++ languages/chs/highlights.scm
@@ -0,0 +1,26 @@
+[
+  "fn"
+  "end"
+  "use"
+  "type"
+  "syscall"
+  "distinct"
+  "if"
+  "else"
+  "while"
+] @keyword
+
+(type_identifier) @type
+
+(integer_literal) @number
+
+[
+  (string_literal)
+  (char_literal)
+] @string
+
+(fn_decl name: (ident) @function)
+(call_expression function: (ident) @function)
+(call_expression arguments: (arguments) @call.inner)
+
+(fn_decl body: (_) @indent)
